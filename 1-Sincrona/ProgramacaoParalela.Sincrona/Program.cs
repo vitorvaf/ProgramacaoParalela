6498dceed73a52fdd6ad31b2136dcfb336e4c580
Vitor Abreu <vitorvaf@gmail.com>
2021-01-24 12:29:38 -0300

    first commit

---
+++ 1-Sincrona/ProgramacaoParalela.Sincrona/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgramacaoParalela.Sincrona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
